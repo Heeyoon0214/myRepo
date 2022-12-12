16bd05009f12bf7ecfab923d3e43558d473dc08a
Heeyoon <107894669+Heeyoon0214@users.noreply.github.com>
2022-12-12 20:26:48 +0900

    :sparkles: 타입 스크립트 사용법

--- New Project/Todo List/src/App.tsx
+++ New Project/Todo List/src/App.tsx
@@ -3,7 +3,28 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const string1: string = "typescript";
+  const string2: string = "testing";
+  const number1: number = 1;
+  const number2: number = 2;
+  function plus(a: number, b: number) {
+    return a + b;
+  }
+  function add(a: string, b: string) {
+    return a + " " + b;
+  }
+
+  return (
+    <div>
+      {string1}
+      <br />
+      {number1}
+      <br />
+      {plus(number1, number2)}
+      <br />
+      {add(string1, string2)}
+    </div>
+  );
 }
 
 export default App;
