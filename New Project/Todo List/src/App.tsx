3d6d133b3ee3fb9447f0a80a4841994a02dcf29c
Heeyoon <107894669+Heeyoon0214@users.noreply.github.com>
2022-12-22 18:29:57 +0900

    :sparkles: add: Tuple

--- New Project/Todo List/src/App.tsx
+++ New Project/Todo List/src/App.tsx
@@ -9,6 +9,9 @@ function App() {
   const number2: number = 2;
   const Do: boolean = true;
   const undo: boolean = false;
+  const list1: number[] = [1, 2, 3];
+  const list2: Array<number> = [4, 5, 6];
+  const list3: string[] = ["A", "B", "C"];
 
   function plus(a: number, b: number) {
     return a + b;
@@ -16,6 +19,9 @@ function App() {
   function add(a: string, b: string) {
     return a + " " + b;
   }
+
+  let x: [string, number];
+  x = ["hello", 10]; // 가능
 
   return (
     <div>
@@ -30,6 +36,12 @@ function App() {
       {Do}
       <br />
       {undo}
+      <br />
+      {list1}
+      <br />
+      {list2}
+      <br />
+      {list3}
     </div>
   );
 }
